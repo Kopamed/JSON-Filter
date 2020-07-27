ffc62291496e5386e2e9faadb003e8a2f1f622cf
K0nDev <kopamedve@gmail.com>
2020-07-27 19:08:44 +0200

    Final Version

--- main.py
+++ main.py
@@ -1,56 +1,19 @@
 import json
-import rd
 
-key_list = ["items"]
-
-class Parser:
+def extract(txt):
     
-    def __init__(self, og_dir, new_dir, key_list = []):
-        
-        self.og_dir = og_dir
-        self.new_dir = new_dir
-        self.key_list = key_list
+    if txt[0] != "{":
+        txt = "{" + txt + "}"
     
+    obj = json.loads(txt)
+    final = {}
     
-    def load_json(self,file_name):
-        
-        with open(file_name, "r") as write_file:
-            
-            data = write_file.read()
-            #data = json.loads(data)
-        
-        
-        if data[0] != "{":
-            with open(file_name, "w") as write_file:
-                
-                write_file.write("{" + data + "}")
-            
-            
-        with open(file_name, "r") as write_file:
-            
-            data = write_file.read()
-            data = json.loads(data)
-            
-        return data
-
-
-
-    def dump_json(self,file_name, data):
-        
-        with open(file_name, "w") as write_file:
-            
-            json.dump(data, write_file)
+    final["tour"] = {"templateOptions": {"price": obj["tour"]["templateOptions"]["price"]},"ttPrice": obj["tour"]["ttPrice"],"totalDays": obj["tour"]["totalDays"], "travelers": obj["tour"]["travelers"], "date": obj["tour"]["date"], "items": {"0": {"currentHub": {"hub": {"id": obj["tour"]["items"]["0"]["currentHub"]["hub"]["id"], "name": obj["tour"]["items"]["0"]["currentHub"]["hub"]["name"], "country": obj["tour"]["items"]["0"]["currentHub"]["hub"]["country"], "countryCode": obj["tour"]["items"]["0"]["currentHub"]["hub"]["countryCode"], "days": {"number": obj["tour"]["items"]["0"]["currentHub"]["hub"]["days"]["number"], "formatted": obj["tour"]["items"]["0"]["currentHub"]["hub"]["days"]["formatted"]}, "nights": {"number": obj["tour"]["items"]["0"]["currentHub"]["hub"]["nights"]["number"], "formatted": obj["tour"]["items"]["0"]["currentHub"]["hub"]["nights"]["formatted"]}, "geolocation": {"0": obj["tour"]["items"]["0"]["currentHub"]["hub"]["geolocation"]["0"] ,"1": obj["tour"]["items"]["0"]["currentHub"]["hub"]["geolocation"]["1"]}, "date": {"startDate": obj["tour"]["items"]["0"]["currentHub"]["hub"]["date"]["startDate"], "endDate": obj["tour"]["items"]["0"]["currentHub"]["hub"]["date"]["endDate"]}}}, "totalHotel": obj["tour"]["items"]["0"]["currentHub"]["totalHotel"], "totalPrice": obj["tour"]["items"]["0"]["currentHub"]["totalPrice"], "totalPriceOnePercon": obj["tour"]["items"]["0"]["currentHub"]["totalPriceOnePercon"], "totalPriceOnePerconFormatted": obj["tour"]["items"]["0"]["currentHub"]["totalPriceOnePerconFormatted"], "totalPricePerDay": obj["tour"]["items"]["0"]["currentHub"]["totalPricePerDay"], "totalPriceActivity": obj["tour"]["items"]["0"]["currentHub"]["totalPriceActivity"]}, "1" : {"currentConnections" : {"id": obj["tour"]["items"]["1"]["currentConnection"]["id"], "entity_id": obj["tour"]["items"]["1"]["currentConnection"]["entity_id"], "name": obj["tour"]["items"]["1"]["currentConnection"]["name"], "type": obj["tour"]["items"]["1"]["currentConnection"]["type"], "duration": obj["tour"]["items"]["1"]["currentConnection"]["duration"], "nights": obj["tour"]["items"]["1"]["currentConnection"]["nights"], "rating": obj["tour"]["items"]["1"]["currentConnection"]["rating"], "fromCity" : obj["tour"]["items"]["1"]["currentConnection"]["fromCity"], "fromHub": obj["tour"]["items"]["1"]["currentConnection"]["fromHub"], "toCity": obj["tour"]["items"]["1"]["currentConnection"]["toCity"], "toHub": obj["tour"]["items"]["1"]["currentConnection"]["toHub"], "geolocation": {"0": obj["tour"]["items"]["1"]["currentConnection"]["geolocation"]["0"], "1": obj["tour"]["items"]["1"]["currentConnection"]["geolocation"]["1"]}, "departureCountryCode": obj["tour"]["items"]["1"]["currentConnection"]["departureCountryCode"], "arrivalCountryCode": obj["tour"]["items"]["1"]["currentConnection"]["arrivalCountryCode"]}}}}
     
     
     
-    def modify_file(self, filename, new_filename="filename"):
+    return  final 
+    
         
-        f = load_json(filename)
-        
-        for i in f:
-            pass
-        
-        
-parser = Parser("defaults", "modified")
 
-print(parser.load_json("defaults/1.json"))
+
